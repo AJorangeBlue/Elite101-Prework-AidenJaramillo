c7afb9868ab010fb4e222c86cbc9c3944607083e
AJorangeBlue <kingaiden17@gmail.com>
2025-09-06 21:06:15 +0000

    First one for elite 101

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-# Thia is elit1 101 pre-work
+# Thia is elite 101 pre-work
 print('Hey there!')
+name = input('What is your name? ')
+
+print(f'Hello {name} you will be doing your Elite 101 Prework')
